6ea65cc0cf039d50fa10424d843257e457b46872
Victordd <854663667@qq.com>
2022-10-13 15:41:47 +0800

    perf: sidebar func

--- js/index.js
+++ js/index.js
@@ -7,8 +7,12 @@ $(() => {
     // });
 
     $('.barL-nav>li>a').click(function () {
-        
+
+        // 打开 or 关闭 选中菜单栏
         $(this).siblings('ul').stop().slideToggle();
+
+        // 当选中菜单打开时，其他关闭
+        $(this).parents().siblings().children('ul').slideUp();
 
     });
 
