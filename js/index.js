13438b88f74919b04d778a71ade54ac5b9651751
Victordd <854663667@qq.com>
2022-10-17 11:17:53 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 $(() => {
 
-    // $(document).ready(() => {
-
-    //     $(".bar").load("./template/barL-nav/barL-nav.html");
-
-    // });
-
     $('.barL-nav>li>a').click(function () {
 
         // 打开 or 关闭 选中菜单栏
